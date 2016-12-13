72758e42858d1c81c5775a9b67f2294ef433774a
Javier De la Hoz <javierdlahoz@gmail.com>
2016-12-13 15:00:06 -0500

    projects and navbar for mobile devices

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -16,20 +16,33 @@ export function NavbarDirective() {
 }
 
 class NavbarController {
-  constructor (moment, $scope, $timeout, $location) {
+  constructor (moment, $scope, $timeout, $location, $mdSidenav) {
     'ngInject';
 
     // "this.creationDate" is available by directive option "bindToController: true"
     this.relativeDate = moment(this.creationDate).fromNow();
     this.isScrolled = false;
     this.init();
-
+    this.$mdSidenav = $mdSidenav;
 
     $scope.$watch('$location.$$hash', () => {
       if($location.hash() == ''){ this.hideFixed(); }
       else { this.showFixed(); }
     });
   }
+
+  toogleMenu() {
+    if(this.isOpenRight()){
+      this.$mdSidenav('right').close()
+    }
+    else {
+      this.$mdSidenav('right').open()
+    }
+  }
+
+  isOpenRight() {
+    return this.$mdSidenav('right').isOpen();
+  };
 
   getElements() {
     this.navbar = document.getElementById("navbar");
